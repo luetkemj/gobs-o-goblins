30b89342eac24112a872050f43612cf1b2a456ca
luetkemj <luetkemj@gmail.com>
2020-06-18 21:53:32 -0500

    moving around and a game loop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-console.log("Hello World!");
+import "./lib/canvas.js";
+
+import { clearCanvas, drawChar } from "./lib/canvas";
+
+const player = {
+  char: "@",
+  color: "white",
+  position: {
+    x: 0,
+    y: 0,
+  },
+};
+
+let userInput = null;
+let playerTurn = false;
+
+document.addEventListener("keydown", (ev) => (userInput = ev.key));
+
+const processUserInput = () => {
+  if (!userInput) {
+    return;
+  }
+
+  if (userInput === "ArrowUp") {
+    player.position.y -= 1;
+  }
+  if (userInput === "ArrowRight") {
+    player.position.x += 1;
+  }
+  if (userInput === "ArrowDown") {
+    player.position.y += 1;
+  }
+  if (userInput === "ArrowLeft") {
+    player.position.x -= 1;
+  }
+};
+
+function gameTick() {
+  clearCanvas();
+  drawChar(player);
+}
+
+function update() {
+  if (userInput && playerTurn) {
+    processUserInput();
+    gameTick();
+    userInput = null;
+    playerTurn = false;
+  }
+
+  if (!playerTurn) {
+    gameTick();
+    playerTurn = true;
+  }
+}
+
+function gameLoop() {
+  update();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
---
+++ src/lib/canvas.js
@@ -0,0 +1,37 @@
+const pixelRatio = window.devicePixelRatio || 1;
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+export const grid = {
+  width: 100,
+  height: 34,
+};
+
+const lineHeight = 1.2;
+
+let calculatedFontSize = window.innerWidth / grid.width;
+let cellWidth = calculatedFontSize * pixelRatio;
+let cellHeight = calculatedFontSize * lineHeight * pixelRatio;
+let fontSize = calculatedFontSize * pixelRatio;
+
+canvas.style.cssText = `width: ${calculatedFontSize * grid.width}; height: ${
+  calculatedFontSize * lineHeight * grid.height
+}`;
+canvas.width = cellWidth * grid.width;
+canvas.height = cellHeight * grid.height;
+
+ctx.font = `normal ${fontSize}px 'Fira Code'`;
+ctx.textAlign = "center";
+ctx.textBaseline = "middle";
+
+export const drawChar = ({ char, color, position }) => {
+  ctx.fillStyle = color;
+  ctx.fillText(
+    char,
+    position.x * cellWidth + cellWidth / 2,
+    position.y * cellHeight + cellHeight / 2
+  );
+};
+
+export const clearCanvas = () =>
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
